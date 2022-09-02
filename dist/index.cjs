c16577b440a244b6dbcaa3b9320b298ca38f8790
Joruno <1710642275@qq.com>
2022-09-02 15:02:20 +0800

    chore: init

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,6 +2,125 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-const a = 1;
+const useTypeOf = function(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+};
+const useDebounce = (() => {
+  let timer;
+  return (cb, wait = 500) => {
+    timer && clearTimeout(timer);
+    timer = setTimeout(cb, wait);
+  };
+})();
+const useThrottle = (() => {
+  let last = 0;
+  return (cb, wait = 500) => {
+    let now = +new Date();
+    if (now - last > wait) {
+      cb();
+      last = now;
+    }
+  };
+})();
+const useHideMobile = (mobile) => {
+  return mobile.replace(/^(\d{3})\d{4}(\d{4})$/, "$1****$2");
+};
+const useLaunchFullscreen = (element) => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullscreen) {
+    element.mozRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  }
+};
+const useExitFullscreen = () => {
+  const doc = document;
+  if (doc.exitFullscreen) {
+    doc.exitFullscreen();
+  } else if (doc.msExitFullscreen) {
+    doc.msExitFullscreen();
+  } else if (doc.mozCancelFullScreen) {
+    doc.mozCancelFullScreen();
+  } else if (doc.webkitExitFullscreen) {
+    doc.webkitExitFullscreen();
+  }
+};
+const useTurnCase = (str, type) => {
+  switch (type) {
+    case 1:
+      return str.toUpperCase();
+    case 2:
+      return str.toLowerCase();
+    case 3:
+      return str[0].toUpperCase() + str.substring(1).toLowerCase();
+    default:
+      return str;
+  }
+};
+const useSearchParams = () => {
+  const searchParams = new URLSearchParams(
+    window.location.search
+  );
+  const params = {};
+  for (const [k, v] of searchParams.entries()) {
+    params[k] = v;
+  }
+  return params;
+};
+const useSysType = () => {
+  const u = navigator.userAgent;
+  const isAndroid = u.indexOf("Android") > -1 || u.indexOf("Linux") > -1;
+  const isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+  if (isIOS) {
+    return "ios";
+  }
+  if (isAndroid) {
+    return "android";
+  }
+  return "";
+};
+const useUniqueArrObj = (arr, key) => {
+  if (!arr.length)
+    return;
+  return Object.values(
+    arr.reduce((acc, item) => {
+      acc[item[key]] || (acc[item[key]] = item);
+    }, {})
+  );
+};
+const useScrollToTop = () => {
+  const height = document.documentElement.scrollTop || document.body.scrollTop;
+  if (height > 0) {
+    window.requestAnimationFrame(useScrollToTop);
+    window.scrollTo(0, height - height / 8);
+  }
+};
+const smoothScroll = (selector) => {
+  const ele = document.querySelector(selector) || document.body;
+  ele.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+const uuid = () => {
+  const temp_url = URL.createObjectURL(new Blob());
+  const uuid2 = temp_url.toString();
+  URL.revokeObjectURL(temp_url);
+  return uuid2.substring(uuid2.lastIndexOf("/") + 1);
+};
 
-exports.a = a;
+exports.smoothScroll = smoothScroll;
+exports.useDebounce = useDebounce;
+exports.useExitFullscreen = useExitFullscreen;
+exports.useHideMobile = useHideMobile;
+exports.useLaunchFullscreen = useLaunchFullscreen;
+exports.useScrollToTop = useScrollToTop;
+exports.useSearchParams = useSearchParams;
+exports.useSysType = useSysType;
+exports.useThrottle = useThrottle;
+exports.useTurnCase = useTurnCase;
+exports.useTypeOf = useTypeOf;
+exports.useUniqueArrObj = useUniqueArrObj;
+exports.uuid = uuid;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,23 @@
-declare const a = 1;
+interface Ele extends Element {
+    requestFullscreen: (options?: FullscreenOptions) => Promise<void>;
+    mozRequestFullscreen?: (options?: FullscreenOptions) => Promise<void>;
+    msRequestFullscreen?: (options?: FullscreenOptions) => Promise<void>;
+    webkitRequestFullscreen?: (options?: FullscreenOptions) => Promise<void>;
+}
+declare type QuerySelector = string | keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap;
 
-export { a };
+declare const useTypeOf: (obj: any) => string;
+declare const useDebounce: (cb: () => void, wait?: number) => void;
+declare const useThrottle: (cb: () => void, wait?: number) => void;
+declare const useHideMobile: (mobile: string) => string;
+declare const useLaunchFullscreen: (element: Ele) => void;
+declare const useExitFullscreen: () => void;
+declare const useTurnCase: (str: string, type: number) => string;
+declare const useSearchParams: () => Record<string, string>;
+declare const useSysType: () => "ios" | "android" | "";
+declare const useUniqueArrObj: <T extends Record<string | number | symbol, any>, U extends T[]>(arr: U, key: keyof T) => unknown[] | undefined;
+declare const useScrollToTop: () => void;
+declare const smoothScroll: (selector: QuerySelector) => void;
+declare const uuid: () => string;
+
+export { smoothScroll, useDebounce, useExitFullscreen, useHideMobile, useLaunchFullscreen, useScrollToTop, useSearchParams, useSysType, useThrottle, useTurnCase, useTypeOf, useUniqueArrObj, uuid };
