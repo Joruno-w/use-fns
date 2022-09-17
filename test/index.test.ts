49dd0ffb4fa4cdbeae2dbc2fe78dd20fedbb4067
Joruno-w <1710642275@qq.com>
2022-09-17 18:45:13 +0800

    feat: add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,14 @@
 import { describe, expect, it } from "vitest";
+import { expectType } from "tsd";
 import { resolve } from "node:path";
+import * as fns from "../src/index";
 
-describe("test", () => {
-  it("test", async () => {
+describe("Get All Export keys", () => {
+  it("exports", async () => {
     expect(Object.keys(await import(resolve(process.cwd(), "./src/index.ts"))))
       .toMatchInlineSnapshot(`
         [
+          "default",
           "useTypeOf",
           "useDebounce",
           "useThrottle",
@@ -34,8 +37,26 @@ describe("test", () => {
           "useShuffle",
           "useGetSelectedText",
           "useGetRandomBoolean",
+          "useSum",
           "useAverage",
+          "useIsUrl",
         ]
       `);
+  });
+});
+
+describe("test each function", () => {
+  it("useIsUrl", () => {
+    expect(fns.useIsUrl("https://github.com")).toBe(true);
+    expect(fns.useIsUrl("github.com")).toBe(false);
+    expect(fns.useIsUrl("github.com", { lenient: true })).toBe(true);
+  });
+});
+
+describe("test each function type declare", () => {
+  it("useIsUrl", () => {
+    expectType<boolean>(fns.useIsUrl("https://github.com"))
+    expectType<boolean>(fns.useIsUrl("github.com"))
+    expectType<boolean>(fns.useIsUrl("github.com", { lenient: true }))
   });
 });
