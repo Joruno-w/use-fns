a10a53de8aa7abfc22ec60a881c0d0cda62cb7fd
Joruno-w <1710642275@qq.com>
2022-09-17 12:58:10 +0800

    feat: add 123

--- src/index.ts
+++ src/index.ts
@@ -286,6 +286,8 @@ const useSum = (arr: any[]) => arr.reduce((a, b) => a + b);
 // 计算数组的平均值
 const useAverage = (arr: any[]) => useSum(arr) / arr.length;
 
+const use123 = 123
+
 
 const pkg = { 
   useTypeOf,
